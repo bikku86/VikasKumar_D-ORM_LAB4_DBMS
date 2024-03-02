bd92f8f22734c7be2f2c65b5a6d320f32dcb066f
VIKAS KUMAR <141577649+bikku86@users.noreply.github.com>
2024-03-02 21:36:29 +0530

    Add files via upload

---
+++ ecommerce.sql
@@ -0,0 +1,371 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 02, 2024 at 04:35 PM
+-- Server version: 10.1.36-MariaDB
+-- PHP Version: 7.0.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ecommerce`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetSupplierInfo` ()  BEGIN
+SELECT supp_id,supp_name, AVG(rat_ratstars),
+Case
+when AVG(rat_ratstars)=5 then 'Excellent Service'
+when AVG(rat_ratstars)>4 then 'Good Service'
+when AVG( rat_ratstars )>2 then 'Average Service'
+else 'Poor Service'
+end as 'Type Of service'
+FROM supplier
+JOIN supplier_pricing USING ( supp_id )
+JOIN orders USING ( pricing_id ) 
+JOIN rating USING ( ord_id ) GROUP BY supp_id;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `supplierDetails` ()  BEGIN
+	SELECT supp_id,supp_name, AVG(rat_ratstars),
+Case
+when AVG(rat_ratstars )=5 then 'Excellent Service'
+when AVG( rat_ratstars )>4 then 'Good Service'
+when AVG( rat_ratstars )>2 then 'Average Service'
+else 'Poor Service'
+end as 'Type Of service'
+FROM supplier
+JOIN supplier_pricing
+USING ( supp_id )
+JOIN orders
+USING ( pricing_id )
+JOIN rating
+USING ( ord_id )
+GROUP BY supp_name;
+	END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `supplierService` ()  BEGIN
+SELECT supp_id,supp_name, AVG(rat_ratstars), 
+Case
+when AVG(rat_ratstars )=5 then 'Excellent Service'
+when AVG( rat_ratstars )>4 then 'Good Service'
+when AVG( rat_ratstars )>2 then 'Average Service' 
+else 'Poor Service' 
+end as 'Type Of service'
+FROM supplier
+JOIN supplier_pricing USING ( supp_id )
+JOIN orders USING ( pricing_id )
+JOIN rating USING ( ord_id ) 
+GROUP BY supp_id; 
+
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `cat_id` int(11) NOT NULL,
+  `cat_name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`cat_id`, `cat_name`) VALUES
+(1, 'BOOKS'),
+(2, 'GAMES'),
+(3, 'GROCERIES'),
+(4, 'ELECTRONICS'),
+(5, 'CLOTHES');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `cus_id` int(11) NOT NULL,
+  `cus_name` varchar(20) NOT NULL,
+  `cus_phone` varchar(10) NOT NULL,
+  `cus_city` varchar(30) NOT NULL,
+  `cus_gender` char(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`cus_id`, `cus_name`, `cus_phone`, `cus_city`, `cus_gender`) VALUES
+(1, 'AAKASH', '9999999999', 'DELHI', 'M'),
+(2, 'AMAN', '9785463215', 'NOIDA', 'M'),
+(3, 'NEHA', '9999999999', 'MUMBAI', 'F'),
+(4, 'MEGHA', '9994562399', 'KOLKATA', 'F'),
+(5, 'PULKIT', '7895999999', 'LUCKNOW', 'M');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `ord_id` int(11) NOT NULL,
+  `ord_amount` int(11) NOT NULL,
+  `ord_date` date NOT NULL,
+  `cus_id` int(11) DEFAULT NULL,
+  `pricing_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`ord_id`, `ord_amount`, `ord_date`, `cus_id`, `pricing_id`) VALUES
+(101, 1500, '2021-10-06', 2, 1),
+(102, 1000, '2021-10-12', 3, 5),
+(103, 30000, '2021-09-16', 5, 2),
+(104, 1500, '2021-10-05', 1, 1),
+(105, 3000, '2021-08-16', 4, 3),
+(106, 1450, '2021-08-18', 1, 9),
+(107, 789, '2021-09-01', 3, 7),
+(108, 780, '2021-09-07', 5, 6),
+(109, 3000, '2021-10-10', 5, 3),
+(110, 2500, '2021-09-10', 2, 4),
+(111, 1000, '2021-09-15', 4, 5),
+(112, 789, '2021-09-16', 4, 7),
+(113, 31000, '2021-09-16', 1, 8),
+(114, 1000, '2021-09-16', 3, 5),
+(115, 3000, '2021-09-16', 5, 3),
+(116, 99, '2021-09-17', 2, 14);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `pro_id` int(11) NOT NULL,
+  `pro_name` varchar(20) NOT NULL DEFAULT 'Dummy',
+  `pro_desc` varchar(60) DEFAULT NULL,
+  `cat_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`pro_id`, `pro_name`, `pro_desc`, `cat_id`) VALUES
+(1, 'GTA V', 'Windows 7 and above with i5 processor and 8GB RAM', 2),
+(2, 'TSHIRT', 'SIZE-L with Black, Blue and White variations', 5),
+(3, 'ROG LAPTOP', 'Windows 10 with 15inch screen, i7 processor, 1TB SSD', 4),
+(4, 'OATS', 'Highly Nutritious from Nestle', 3),
+(5, 'HARRY POTTER', 'Best Collection of all time by J.K Rowling', 1),
+(6, 'MILK 1L', 'Toned Milk', 3),
+(7, 'Boat Earphones', '1.5 Meter long Dolby Atmos', 4),
+(8, 'Jeans', 'Stretchable Denim Jeans with various sizes and color', 5),
+(9, 'Project IGI', 'Compatible with Windows 7 and above', 2),
+(10, 'Hoodie', 'Black GUCCI for 13 yrs and above', 5),
+(11, 'Rich Dad Poor Dad', 'Written by Robert Kiyosaki', 1),
+(12, 'Train Your Brain', 'By Shireen Stephen', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rating`
+--
+
+CREATE TABLE `rating` (
+  `rat_id` int(11) NOT NULL,
+  `ord_id` int(11) DEFAULT NULL,
+  `rat_ratstars` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `rating`
+--
+
+INSERT INTO `rating` (`rat_id`, `ord_id`, `rat_ratstars`) VALUES
+(1, 101, 4),
+(2, 102, 3),
+(3, 103, 1),
+(4, 104, 2),
+(5, 105, 4),
+(6, 106, 3),
+(7, 107, 4),
+(8, 108, 4),
+(9, 109, 3),
+(10, 110, 5),
+(11, 111, 3),
+(12, 112, 4),
+(13, 113, 2),
+(14, 114, 1),
+(15, 115, 1),
+(16, 116, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `supp_id` int(11) NOT NULL,
+  `supp_name` varchar(50) NOT NULL,
+  `supp_city` varchar(50) NOT NULL,
+  `supp_phone` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `supplier`
+--
+
+INSERT INTO `supplier` (`supp_id`, `supp_name`, `supp_city`, `supp_phone`) VALUES
+(1, 'Rajesh Retails', 'Delhi', '1234567890'),
+(2, 'Appario Ltd.', 'Mumbai', '2589631470'),
+(3, 'Knome products', 'Banglore', '9785462315'),
+(4, 'Bansal Retails', 'Kochi', '8975463285'),
+(5, 'Mittal Ltd.', 'Lucknow', '7898456532');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier_pricing`
+--
+
+CREATE TABLE `supplier_pricing` (
+  `pricing_id` int(11) NOT NULL,
+  `pro_id` int(11) DEFAULT NULL,
+  `supp_id` int(11) DEFAULT NULL,
+  `supp_price` int(11) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `supplier_pricing`
+--
+
+INSERT INTO `supplier_pricing` (`pricing_id`, `pro_id`, `supp_id`, `supp_price`) VALUES
+(1, 1, 2, 1500),
+(2, 3, 5, 30000),
+(3, 5, 1, 3000),
+(4, 2, 3, 2500),
+(5, 4, 1, 1000),
+(6, 12, 2, 780),
+(7, 12, 4, 789),
+(8, 3, 1, 31000),
+(9, 1, 5, 1450),
+(10, 4, 2, 999),
+(11, 7, 3, 549),
+(12, 7, 4, 529),
+(13, 6, 2, 105),
+(14, 6, 1, 99),
+(15, 2, 5, 2999),
+(16, 5, 2, 2999);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`cus_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`ord_id`),
+  ADD KEY `cus_id` (`cus_id`),
+  ADD KEY `pricing_id` (`pricing_id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`pro_id`),
+  ADD KEY `cat_id` (`cat_id`);
+
+--
+-- Indexes for table `rating`
+--
+ALTER TABLE `rating`
+  ADD PRIMARY KEY (`rat_id`),
+  ADD KEY `ord_id` (`ord_id`);
+
+--
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`supp_id`);
+
+--
+-- Indexes for table `supplier_pricing`
+--
+ALTER TABLE `supplier_pricing`
+  ADD PRIMARY KEY (`pricing_id`),
+  ADD KEY `pro_id` (`pro_id`),
+  ADD KEY `supp_id` (`supp_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`cus_id`) REFERENCES `customer` (`cus_id`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`pricing_id`) REFERENCES `supplier_pricing` (`pricing_id`);
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`cat_id`) REFERENCES `category` (`cat_id`);
+
+--
+-- Constraints for table `rating`
+--
+ALTER TABLE `rating`
+  ADD CONSTRAINT `rating_ibfk_1` FOREIGN KEY (`ord_id`) REFERENCES `orders` (`ord_id`);
+
+--
+-- Constraints for table `supplier_pricing`
+--
+ALTER TABLE `supplier_pricing`
+  ADD CONSTRAINT `supplier_pricing_ibfk_1` FOREIGN KEY (`pro_id`) REFERENCES `product` (`pro_id`),
+  ADD CONSTRAINT `supplier_pricing_ibfk_2` FOREIGN KEY (`supp_id`) REFERENCES `supplier` (`supp_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
